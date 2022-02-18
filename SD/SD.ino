4add967de888dbbf72a22890af989c926d589d71
elPytel <jaroslav.korner1@gmail.com>
2022-02-18 13:09:48 +0100

    SD test

--- SD/SD.ino
+++ SD/SD.ino
@@ -1,10 +1,43 @@
 #define FORCE_SENSOR_PIN A5
-#define SEL 4
+#define SEL 5
+#define BUTON 6
 #define TIME 1
+
+/*
+  SD card read/write
+
+  This example shows how to read and write data to and from an SD card file
+  The circuit:
+   SD card attached to SPI bus as follows:
+ ** MOSI - pin 11
+ ** MISO - pin 12
+ ** CLK - pin 13
+ ** CS - pin 4 (for MKRZero SD: SDCARD_SS_PIN)
+
+  created   Nov 2010
+  by David A. Mellis
+  modified 9 Apr 2012
+  by Tom Igoe
+
+  This example code is in the public domain.
+
+*/
+
+#include <SPI.h>
+#include <SD.h>
+
+File myFile;
+
+void setupSD();
+void testSD();
 
 void setup() {
   Serial.begin(9600);
   pinMode(SEL, OUTPUT);
+  pinMode(BUTON, INPUT_PULLUP);
+  setupSD();
+  testSD();
+  delay(10000);
 }
 
 void loop() {
@@ -20,4 +53,53 @@ void loop() {
   Serial.print("\t Force dumy reading = ");
   Serial.println(analogReading);
   delay(TIME);
+}
+
+void setupSD () {
+  // Open serial communications and wait for port to open:
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
+
+  Serial.print("Initializing SD card...");
+
+  if (!SD.begin(4)) {
+    Serial.println("initialization failed!");
+    while (1);
+  }
+  Serial.println("initialization done.");
+}
+
+void testSD () {
+  // open the file. note that only one file can be open at a time,
+  // so you have to close this one before opening another.
+  myFile = SD.open("test.txt", FILE_WRITE);
+
+  // if the file opened okay, write to it:
+  if (myFile) {
+    Serial.print("Writing to test.txt...");
+    myFile.println("testing 1, 2, 3.");
+    // close the file:
+    myFile.close();
+    Serial.println("done.");
+  } else {
+    // if the file didn't open, print an error:
+    Serial.println("error opening test.txt");
+  }
+
+  // re-open the file for reading:
+  myFile = SD.open("test.txt");
+  if (myFile) {
+    Serial.println("test.txt:");
+
+    // read from the file until there's nothing else in it:
+    while (myFile.available()) {
+      Serial.write(myFile.read());
+    }
+    // close the file:
+    myFile.close();
+  } else {
+    // if the file didn't open, print an error:
+    Serial.println("error opening test.txt");
+  }
 }
